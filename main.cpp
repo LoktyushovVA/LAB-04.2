fd2449087cc7f83efe516649734997c144e470fc
LoktyushovVA <Loktyushov-vl@email.ru>
2019-05-20 22:21:15 +0300

    code:выполнили пункт :если содержит двоеточие, то только вторым символом, которому предшествует буква, и за двоеточием следует обратная косая черта (\)

--- main.cpp
+++ main.cpp
@@ -9,10 +9,41 @@ const char* forbidden =  "*<>?|\"";
 
 int main ()
 {
+int main()
+{
     char text[MAX_SIZE];
-    cout<<"Enter the name of the file :";
-    //STDIN
+    cout << "Enter file name: ";
     fgets(text, MAX_SIZE, stdin);
     char* namefile = text;
+    while(true)
+    {
+        if( strcspn( namefile, forbidden ) == strlen( namefile ) ) ;
+        else
+        {
+            cout << "Wrong format\n";
+            break;
+        }
 
-}
+        if( strchr(namefile, ':' ) == nullptr ) ;
+        else
+        {
+            char* colon = strchr(namefile,':');
+            if( colon - namefile + 1 <= 1 )
+            {
+                cout << ""Wrong format";
+                break;
+            }
+
+                        while( colon != nullptr )
+            {
+                if( isalpha( filename[colon - filename - 1] ) > 0 &&
+                    filename[colon - filename + 1] == '\\')
+                    colon = strchr(colon + 1, ':');
+                else
+                {
+                    cout << "Invalid format";
+                    break;
+                }
+            }
+        }
+
