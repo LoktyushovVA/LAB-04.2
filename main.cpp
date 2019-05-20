46a11437537dae5261b295353e0d98047e70f089
LoktyushovVA <Loktyushov-vl@email.ru>
2019-05-20 22:30:53 +0300

    code:Использововали fopen() для доступа к файлам

--- main.cpp
+++ main.cpp
@@ -30,20 +30,43 @@ int main()
             char* colon = strchr(namefile,':');
             if( colon - namefile + 1 <= 1 )
             {
-                cout << ""Wrong format";
+                cout << "Wrong format";
                 break;
             }
 
                         while( colon != nullptr )
             {
-                if( isalpha( filename[colon - filename - 1] ) > 0 &&
-                    filename[colon - filename + 1] == '\\')
+                if( isalpha( namefile[colon - namefile - 1] ) > 0 &&
+                    namefile[colon - namefile + 1] == '\\')
                     colon = strchr(colon + 1, ':');
                 else
                 {
-                    cout << "Invalid format";
+                    cout << "Wrong format";
                     break;
                 }
             }
         }
+        if( strrchr( namefile, '.' ) == nullptr )
+        {
+            *(namefile + strlen(namefile) - 1) = '.';
+            strcat( namefile, "txt" );
+        }
 
+        else
+        {
+            char* ext = strrchr( namefile, '.' );
+            for( int i = 1; ext[i] != '\0'; i ++ )
+                *(ext + i) = char(tolower(*(ext + i)));
+            if( strncmp( ext, ".txt", MAX_EXTENSION + 1 ) == 0 );
+            else
+            {
+                cout << "Wrong format";
+                break;
+            }
+        }
+        break;
+    }
+
+     FILE* file = fopen( namefile, "r" );
+    fseek( file, 0, SEEK_END );
+    long filesize = ftell( file );
